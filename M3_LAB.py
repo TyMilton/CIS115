1f38c586c2d28b5384a4cb340968366157064823
TyMilton <43816225+TyMilton@users.noreply.github.com>
2018-10-14 13:21:26 -0400

    Add files via upload

---
+++ M3_LAB.py
@@ -0,0 +1,49 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+Traceback (most recent call last):
+  File "C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py", line 105, in <module>
+    main()
+  File "C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py", line 91, in main
+    fd(bob, radius)
+NameError: name 'radius' is not defined
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+Traceback (most recent call last):
+  File "C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py", line 105, in <module>
+    main()
+  File "C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py", line 91, in main
+    fd(bob, radius)
+NameError: name 'radius' is not defined
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+Traceback (most recent call last):
+  File "C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py", line 109, in <module>
+    main()
+  File "C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py", line 97, in main
+    bd(bob, 50)
+NameError: name 'bd' is not defined
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
+======== RESTART: C:\Users\tyler\Desktop\Programming\M3LAB\polygon.py ========
+>>> 
